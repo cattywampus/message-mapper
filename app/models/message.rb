b00ee401d31dab8c350cb539abf5dca789e0402b
Keith Walters <keith.walters@cattywamp.us>
2016-08-10 21:12:51 -0400

    Fixes `Message#producers` association
    Specifying the join table for habtm tells rails which table to use for
    the association foreign keys.

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,4 @@
 class Message < ApplicationRecord
   belongs_to :baseline
-  has_and_belongs_to_many :producers, class_name: Application
+  has_and_belongs_to_many :producers, join_table: :producers, class_name: Application
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+product = Product.find_or_create_by name: "Program A" do |product|
+  product.description = "My Program/Product"
+end
+
+baseline = Baseline.find_or_create_by name: "Build Z" do |baseline|
+  baseline.product = product
+end
+
+Message.find_or_create_by msg_id: "12345" do |message|
+  message.description = "This is a test message"
+  message.baseline = baseline
+end
+
+Application.find_or_create_by name: "App 1" do |application|
+  application.baseline = baseline
+  application.description = "Application running in this baseline"
+end
