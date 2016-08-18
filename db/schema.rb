67ecb97d5fcc46cc7d8567d34e31da17f39f31ab
Matt Shildt <matt.shildt@gmail.com>
2016-08-17 21:06:56 -0400

    Updating new message form.
    Still a work in progess.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160813181022) do
+ActiveRecord::Schema.define(version: 20160816004942) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20160813181022) do
     t.integer  "baseline_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "name"
     t.index ["baseline_id"], name: "index_messages_on_baseline_id", using: :btree
   end
 
